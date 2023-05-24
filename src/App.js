28e0b7a39800be417141c291a8fde344e928dc7a
issac-oh <issac1162@gmail.com>
2023-05-24 20:17:59 +0900

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("run~~");
+  useEffect(() => {
+    console.log("API 호출");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>
-        Welcome back!
-      </h1>
-      <Button text={"Button"}/>
+      <input 
+        value={keyword} 
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+        />
+        <h1>{counter}</h1>
+        <button onClick={onClick}>Click This</button>
     </div>
   );
 }
